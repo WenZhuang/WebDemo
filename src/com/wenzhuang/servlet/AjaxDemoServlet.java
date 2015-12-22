203a5c0336b6257655b10a2ee55bb8ae842b03a4
WenZhuang <wenzhuang2012@gmail.com>
2015-12-22 15:19:07 +0800

    add ajaxDemo with jQuery

---
+++ src/com/wenzhuang/servlet/AjaxDemoServlet.java
@@ -0,0 +1,103 @@
+package com.wenzhuang.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+
+public class AjaxDemoServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public AjaxDemoServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		JSONObject jsonObject = new JSONObject();
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("name", "wenzhuang");
+		map.put("task", "Java");
+		jsonObject = JSONObject.fromObject(map);
+		
+		out.println(jsonObject);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.print(username + " " + password);
+		JSONObject jsonObject = new JSONObject();
+		if(username.equals("wenzhuang") && password.equals("123")){
+			jsonObject.put("state", 1);
+			jsonObject.put("msg", "验证通过！");
+		}else {
+			jsonObject.put("state", 0);
+			jsonObject.put("msg", "用户名或密码错误！");
+		}
+		out.print(jsonObject);
+		
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
